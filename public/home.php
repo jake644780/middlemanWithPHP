ccbcfd722d14beb0f74a3f2eb6d2b71a3123cf76
jake644780 <yesn7722@gmail.com>
2024-10-20 22:22:05 +0200

    navbar making1

---
+++ public/home.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>middleman</title>        
+    </head>
+<body>
+
+<div class="navbar">
+        <div class="navbar-element">home</div>
+        <div class="navbar-element">shop</div>
+        <div class="navbar-element">*place of the searchbar*</div>
+        <div class="navbar-element">main page</div>
+        <div class="navbar-element">login/logout</div>
+        <div class="navbar-element">singup/myaccount</div>
+    </div>
+
+    
+      
+</body>
+
+</html>
