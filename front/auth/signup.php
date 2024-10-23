6383ed931991d0cc4645a7a3681a960564f97f86
jake644780 <yesn7722@gmail.com>
2024-10-23 11:01:51 +0200

    added pass-repass check

--- front/auth/signup.php
+++ front/auth/signup.php
@@ -7,7 +7,7 @@
     username: <input type="text" name="username"><br>
     email: <input type="text" name="email"><br>
     password: <input type="password" name="password"><br>
-    repeat password: <input type="password"><br>
+    repeat password: <input type="password" name="repass"><br>
     password requirements:
     <ul><li>between the length of 8 and 40</li><li>atleast 1 number</li><li>atleast 1 uppercase letter</li></ul><br>
     tell us a bit about yourself: <br>
@@ -56,18 +56,21 @@
         if ($password_validator != 1){
             jsLog(getPassErrs($password_validator)); //frontend this
         }else{
-            
-            //checking if user is already present in db
-            $isUserInDB_RESULT = $conn->query(QselectAllByUsername($data["username"]));
+            if ($data["password"] != @$_POST["repass"]){
+               jsLog("password does not match!"); // frontend this
+            } else{
+                 //checking if user is already present in db
+                 $isUserInDB_RESULT = $conn->query(QselectAllByUsername($data["username"]));
 
-            if ($isUserInDB_RESULT->num_rows != 0){
-                jsLog("user with this username already exists"); //frontend this
-            }else{
-                $insertUser_RESULT = $conn->query(QinsertWithHash("users", $data));
-
-                if ($insertUser_RESULT){
-                    jsLog("successfully registered user"); //frontend this
-                }
+                 if ($isUserInDB_RESULT->num_rows != 0){
+                     jsLog("user with this username already exists"); //frontend this
+                 }else{
+                     $insertUser_RESULT = $conn->query(QinsertWithHash("users", $data));
+ 
+                     if ($insertUser_RESULT){
+                         jsLog("successfully registered user"); //frontend this
+                     }
+                 }
             }
         }
     }
