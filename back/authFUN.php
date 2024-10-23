6ba0edb02e73cd15f2255a5923ec637ddb62c8a5
jake644780 <yesn7722@gmail.com>
2024-10-23 11:27:50 +0200

    Fix this note added

--- back/authFUN.php
+++ back/authFUN.php
@@ -8,7 +8,7 @@ function checkPass($password) {
     return [$len, $cap, $num];
 }
 
-function getPassErrs($errors){
+function getPassErrs($errors){ //FIX THIS WITH JSLOGGING!!!!!!
     $passwordError = "password has to match these expectations:\n";
     $expectations = ["must have a length between 8 and 40","must have atleast 1 capital letter","must have atleast 1 numerical"];
     for ($i = 0; $i < count($expectations); $i++) if ($errors[$i] == 0) $passwordError .= $i + 1 . "." . $expectations[$i];
